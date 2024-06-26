65f8ebd6f4afb3e62826eb7ccef3c2d506198065
Christian Bongiorno <christian.bongiorno@centralesupelec.fr>
2024-06-26 15:53:03 +0200

    chore: Refactor IrreversibilityEstimator class to include test data in training

--- irreversibility_estimator/irreversibility_estimator.py
+++ irreversibility_estimator/irreversibility_estimator.py
@@ -14,14 +14,12 @@ class IrreversibilityEstimator:
     verbose (bool): If True, print progress messages. Default is False.
     interaction_constraints (str): Constraints on interactions between features as a string.
     random_state (int or None): Seed for random number generator. Default is None.
-    kf (KFold or GroupKFold): KFold or GroupKFold cross-validator.
     
     Methods:
-    - prepare_data(self, x_forward, x_backward=None): Prepares the forward and backward datasets.
-    - train(self, x_forward, x_backward, train_index): Trains the model on the training set and returns the trained model.
+    - train(self, x_forward, x_backward, train_index,test_index): Trains the model on the training set and returns the trained model.
     - evaluate(self, model, x_forward, x_backward, test_index, return_log_diffs=False): Evaluates the model on the test set and returns the irreversibility.
     - train_and_evaluate(self, x_forward, x_backward, train_index, test_index, return_log_diffs=False): Trains the model and evaluates it on the test set for a single fold.
-    - fit_predict(self, x_forward, x_backward=None,n_splits=5, groups=None): Performs k-fold or group k-fold cross-validation to estimate irreversibility.
+    - fit_predict(self, x_forward, x_backward=None,n_splits=5, groups=None, return_log_diffs=False): Performs k-fold or group k-fold cross-validation to estimate irreversibility.
     
     Example:
     ```python
@@ -72,7 +70,7 @@ class IrreversibilityEstimator:
         self.interaction_constraints = interaction_constraints
         self.random_state = random_state
         
-    def prepare_data(self, x_forward, x_backward=None):
+    def _prepare_data(self, x_forward, x_backward=None):
         """
         Prepares the forward and backward datasets.
         
@@ -87,23 +85,26 @@ class IrreversibilityEstimator:
             x_backward = x_forward[:, ::-1]
         return x_forward, x_backward
     
-    def train(self, x_forward, x_backward, train_index,test_index):
+    def train(self, x_forward_train, x_backward_train, x_forward_test=None, x_backward_test=None):
         """
         Trains the model on the training set and returns the trained model.
         
         Args:
-        x_forward (ndarray): Encodings of the forward trajectories.
-        x_backward (ndarray): Encodings of the backward trajectories.
-        train_index (ndarray): Indices for the training set.
+        x_forward_train (ndarray): Encodings of the forward trajectories in the training set.
+        x_backward_train (ndarray): Encodings of the backward trajectories in the training set.
+        x_forward_test (ndarray, optional): Encodings of the forward trajectories in the test set. Default is None.
+        x_backward_test (ndarray, optional): Encodings of the backward trajectories in the test set. Default is None.
         
         Returns:
         XGBClassifier: Trained XGBoost model.
         """
-        y_train = np.r_[np.ones_like(train_index), np.zeros_like(train_index)]
-        X_train = np.row_stack((x_forward[train_index], x_backward[train_index]))
+        y_train = np.r_[np.ones_like(x_forward_train), np.zeros_like(x_backward_train)]
+        X_train = np.row_stack((x_forward_train, x_backward_train))
 
-        y_test = np.r_[np.ones_like(test_index), np.zeros_like(test_index)]
-        X_test = np.row_stack((x_forward[test_index], x_backward[test_index]))
+        if x_forward_test is not None and x_backward_test is not None:
+            y_test = np.r_[np.ones_like(x_forward_test), np.zeros_like(x_backward_test)]
+            X_test = np.row_stack((x_forward_test, x_backward_test))
+
 
         model = xgb.XGBClassifier(
             n_estimators=self.n_estimators,
@@ -116,11 +117,18 @@ class IrreversibilityEstimator:
         if self.verbose:
             print(f"Training model with train size {len(train_index)}")
 
-        model.fit(X_train, y_train, verbose=self.verbose, eval_set=[(X_test, y_test)])
+        if x_forward_test is not None and x_backward_test is not None:
+            model.fit(X_train, y_train, verbose=self.verbose, eval_set=[(X_test, y_test)])
+            # if the early stopping rounds are not reached, and the last iteration is equal the number of trees, write a warning
+            if model.get_booster().num_boosted_rounds() == self.n_estimators:
+                print('Warning: early stopping rounds not reached. Consider increasing the number of trees.')
+        else:
+            model.fit(X_train, y_train, verbose=self.verbose)
+
         
         return model
     
-    def evaluate(self, model, x_forward, x_backward, test_index, return_log_diffs=False):
+    def evaluate(self, model, x_forward, x_backward, return_log_diffs=False):
         """
         Evaluates the model on the test set and returns the irreversibility.
         
@@ -128,15 +136,14 @@ class IrreversibilityEstimator:
         model: Any model compatible with the scikit-learn API.
         x_forward (ndarray): Encodings of the forward trajectories.
         x_backward (ndarray): Encodings of the backward trajectories.
-        test_index (ndarray): Indices for the test set.
         return_log_diffs (bool): If True, return the individual log differences of the probabilities. Default is False.
         
         Returns:
         float: Calculated irreversibility for the test set.
         list: Individual log differences of the probabilities, if return_log_diffs is True.
         """
-        y_test = np.r_[np.ones_like(test_index), np.zeros_like(test_index)]
-        X_test = np.row_stack((x_forward[test_index], x_backward[test_index]))
+        y_test = np.r_[np.ones_like(x_forward), np.zeros_like(x_backward)]
+        X_test = np.row_stack((x_forward, x_backward))
 
         prob = model.predict_proba(X_test)[:, 1]
 
@@ -166,8 +173,8 @@ class IrreversibilityEstimator:
         float: Calculated irreversibility for the fold.
         list: Individual log differences of the probabilities, if return_log_diffs is True.
         """
-        model = self.train(x_forward, x_backward, train_index,test_index)
-        return self.evaluate(model, x_forward, x_backward, test_index, return_log_diffs)
+        model = self.train(x_forward[train_index], x_backward[train_index], x_forward[test_index], x_backward[test_index])
+        return self.evaluate(model, x_forward[test_index], x_backward[test_index], return_log_diffs)
     
     def fit_predict(self, x_forward, x_backward=None,n_splits=5, groups=None,return_log_diffs=False):
         """
--- test.py
+++ test.py
@@ -2,8 +2,12 @@
 import irreversibility_estimator as ie
 import numpy as np
 
-estimator = ie.IrreversibilityEstimator()
-x_forward = np.random.normal(0.6,1,size=(1000,2))
+estimator = ie.IrreversibilityEstimator
+mean = 0.6
+std = 1
+x_forward = np.random.normal(mean,std,size=(10000,2))
 x_backward = -x_forward[:,::-1]
 
 print(estimator.fit_predict(x_forward, x_backward))
+#expected KL divergence value
+print( x_forward.shape[1]*2*(mean/std)**2 )
