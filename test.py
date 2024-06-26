28c1fe63742082eaa71741d23ea94568c266447f
Christian Bongiorno <christian.bongiorno@centralesupelec.fr>
2024-06-26 16:17:53 +0200

    Refactor IrreversibilityEstimator class to include test data in training and update .gitignore

--- test.py
+++ test.py
@@ -11,3 +11,8 @@ x_backward = -x_forward[:,::-1]
 print(estimator.fit_predict(x_forward, x_backward))
 #expected KL divergence value
 print( x_forward.shape[1]*2*(mean/std)**2 )
+
+print(estimator.fit_predict(x_forward, return_log_diffs=True))
+
+groups = np.random.randint(0,6,10000)
+print(estimator.fit_predict(x_forward, x_backward, groups=groups))
