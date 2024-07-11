79fd0902e3c59b21bea6d25c4ac340d0879d983e
Christian Bongiorno <christian.bongiorno@centralesupelec.fr>
2024-07-11 08:03:32 +0200

    chore: Bump version to 0.4.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='time_irreversibility_estimator',
-    version='0.4',
+    version='0.4.1',
     packages=find_packages(),
     install_requires=[
         'numpy>=2.0.0',
